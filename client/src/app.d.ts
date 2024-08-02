292c5c4781e5b4713d489856b3773ea193bf3503
Luka Csurgay <luka.csurgay@accedo.tv>
2024-08-02 17:46:58 +0200

    add types for d3, dtree

--- client/src/app.d.ts
+++ client/src/app.d.ts
@@ -1,13 +1,51 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+
+declare module 'd3-dtree' {
+  interface Person {
+    name: string;
+  }
+  interface Marriages {
+    spouse: Person;
+    children: Person[];
+  }
+  interface DataObject {
+    name: string;
+    class: string;
+    textClass: string;
+    depthOffset: number;
+    marriages: Marriages[];
+    extra?: Record<string, any>;
+  }
+
+  interface Options {
+    target: 'string';
+    debug: boolean;
+    width: number;
+    height: number;
+    hideMarriageNodes: boolean;
+    marriageNodeSize: number;
+    callbacks: {
+      /*
+          Callbacks should only be overwritten on a need to basis.
+          See the section about callbacks below.
+        */
+    };
+    margin: {
+      top: number;
+      right: number;
+      bottom: number;
+      left: number;
+    };
+    nodeWidth: number;
+    styles: {
+      node: string;
+      linage: string;
+      marriage: string;
+      text: string;
+    };
+  }
+
+  var init: (data: DataObject[], options?: Options) => void;
 }
 
-export {};
