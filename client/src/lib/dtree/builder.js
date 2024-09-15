bcffb833b5fe19bdbf1ad7ea65f07239266687ea
Luka Csurgay <lukacsurgay@gmail.com>
2024-09-15 17:17:54 +0200

    move dtree to lib

--- client/src/lib/dtree/builder.js
+++ client/src/lib/dtree/builder.js
@@ -1,5 +1,5 @@
+// @ts-nocheck
 class TreeBuilder {
-
   constructor(root, siblings, opts) {
     TreeBuilder.DEBUG_LEVEL = opts.debug ? 1 : 0;
 
@@ -11,27 +11,28 @@ class TreeBuilder {
     this.allNodes = this._flatten(this.root);
 
     // calculate node sizes
-    this.nodeSize = opts.callbacks.nodeSize.call(this,
+    this.nodeSize = opts.callbacks.nodeSize.call(
+      this,
       // filter hidden and marriage nodes
       _.filter(
         this.allNodes,
-        node => !(node.hidden || _.get(node, 'data.isMarriage'))
+        (node) => !(node.hidden || _.get(node, 'data.isMarriage')),
       ),
       opts.nodeWidth,
-      opts.callbacks.textRenderer
-    )
-    this.marriageSize = opts.callbacks.marriageSize.call(this,
+      opts.callbacks.textRenderer,
+    );
+    this.marriageSize = opts.callbacks.marriageSize.call(
+      this,
       // filter hidden and non marriage nodes
       _.filter(
         this.allNodes,
-        node => !node.hidden && _.get(node, 'data.isMarriage')
+        (node) => !node.hidden && _.get(node, 'data.isMarriage'),
       ),
-      this.opts.marriageNodeSize
-    )
+      this.opts.marriageNodeSize,
+    );
   }
 
   create() {
-
     let opts = this.opts;
     let allNodes = this.allNodes;
     let nodeSize = this.nodeSize;
@@ -40,28 +41,40 @@ class TreeBuilder {
     let height = opts.height + opts.margin.top + opts.margin.bottom;
 
     // create zoom handler
-    const zoom = this.zoom = d3.zoom()
+    const zoom = (this.zoom = d3
+      .zoom()
       .scaleExtent([0.1, 10])
       .on('zoom', function () {
-        g.attr('transform', d3.event.transform)
-      })
+        g.attr('transform', d3.event.transform);
+      }));
 
     // make a svg
-    const svg = this.svg = d3.select(opts.target)
+    const svg = (this.svg = d3
+      .select(opts.target)
       .append('svg')
       .attr('viewBox', [0, 0, width, height])
-      .call(zoom)
+      .call(zoom));
 
     // create svg group that holds all nodes
-    const g = this.g = svg.append('g')
+    const g = (this.g = svg.append('g'));
 
     // set zoom identity
-    svg.call(zoom.transform, d3.zoomIdentity.translate(width / 2, opts.margin.top).scale(1))
+    svg.call(
+      zoom.transform,
+      d3.zoomIdentity.translate(width / 2, opts.margin.top).scale(1),
+    );
 
     // Compute the layout.
-    this.tree = d3.tree()
-      .nodeSize([nodeSize[0] * 2,
-                 opts.callbacks.nodeHeightSeperation.call(this, nodeSize[0], nodeSize[1])]);
+    this.tree = d3
+      .tree()
+      .nodeSize([
+        nodeSize[0] * 2,
+        opts.callbacks.nodeHeightSeperation.call(
+          this,
+          nodeSize[0],
+          nodeSize[1],
+        ),
+      ]);
 
     this.tree.separation(function separation(a, b) {
       if (a.data.hidden || b.data.hidden) {
@@ -72,11 +85,9 @@ class TreeBuilder {
     });
 
     this._update(this.root);
-
   }
 
   _update(source) {
-
     let opts = this.opts;
     let allNodes = this.allNodes;
     let nodeSize = this.nodeSize;
@@ -86,25 +97,25 @@ class TreeBuilder {
     let links = treenodes.links();
 
     // Create the link lines.
-    this.g.selectAll('.link')
+    this.g
+      .selectAll('.link')
       .data(links)
       .enter()
       // filter links with no parents to prevent empty nodes
-      .filter(function(l) {
+      .filter(function (l) {
         return !l.target.data.noParent;
       })
       .append('path')
       .attr('class', opts.styles.linage)
       .attr('d', this._elbow);
 
-    let nodes = this.g.selectAll('.node')
-      .data(treenodes.descendants())
-      .enter();
+    let nodes = this.g.selectAll('.node').data(treenodes.descendants()).enter();
 
     this._linkSiblings();
 
     // Draw siblings (marriage)
-    this.g.selectAll('.sibling')
+    this.g
+      .selectAll('.sibling')
       .data(this.siblings)
       .enter()
       .append('path')
@@ -112,38 +123,41 @@ class TreeBuilder {
       .attr('d', _.bind(this._siblingLine, this));
 
     // Create the node rectangles.
-    nodes.append('foreignObject')
-      .filter(function(d) {
+    nodes
+      .append('foreignObject')
+      .filter(function (d) {
         return d.data.hidden ? false : true;
       })
-      .attr('x', function(d) {
+      .attr('x', function (d) {
         return Math.round(d.x - d.cWidth / 2) + 'px';
       })
-      .attr('y', function(d) {
+      .attr('y', function (d) {
         return Math.round(d.y - d.cHeight / 2) + 'px';
       })
-      .attr('width', function(d) {
+      .attr('width', function (d) {
         return d.cWidth + 'px';
       })
-      .attr('height', function(d) {
+      .attr('height', function (d) {
         return d.cHeight + 'px';
       })
-      .attr('id', function(d) {
+      .attr('id', function (d) {
         return d.id;
       })
-      .html(function(d) {
+      .html(function (d) {
         if (d.data.isMarriage) {
-          return opts.callbacks.marriageRenderer.call(this,
+          return opts.callbacks.marriageRenderer.call(
+            this,
             d.x,
             d.y,
             marriageSize[0],
             marriageSize[1],
             d.data.extra,
             d.data.id,
-            d.data.class
-          )
+            d.data.class,
+          );
         } else {
-          return opts.callbacks.nodeRenderer.call(this,
+          return opts.callbacks.nodeRenderer.call(
+            this,
             d.data.name,
             d.x,
             d.y,
@@ -153,35 +167,45 @@ class TreeBuilder {
             d.data.id,
             d.data.class,
             d.data.textClass,
-            opts.callbacks.textRenderer
-          )
+            opts.callbacks.textRenderer,
+          );
         }
       })
       .on('dblclick', function () {
         // do not propagate a double click on a node
         // to prevent the zoom from being triggered
-        d3.event.stopPropagation()
+        d3.event.stopPropagation();
       })
-      .on('click', function(d)  {
+      .on('click', function (d) {
         // ignore double-clicks and clicks on hidden nodes
         if (d3.event.detail === 2 || d.data.hidden) {
           return;
         }
         if (d.data.isMarriage) {
-          opts.callbacks.marriageClick.call(this, d.data.extra, d.data.id)
+          opts.callbacks.marriageClick.call(this, d.data.extra, d.data.id);
         } else {
-          opts.callbacks.nodeClick.call(this, d.data.name, d.data.extra, d.data.id)
+          opts.callbacks.nodeClick.call(
+            this,
+            d.data.name,
+            d.data.extra,
+            d.data.id,
+          );
         }
       })
-      .on('contextmenu', function(d)  {
+      .on('contextmenu', function (d) {
         if (d.data.hidden) {
           return;
         }
         d3.event.preventDefault();
         if (d.data.isMarriage) {
-          opts.callbacks.marriageRightClick.call(this, d.data.extra, d.data.id)
+          opts.callbacks.marriageRightClick.call(this, d.data.extra, d.data.id);
         } else {
-          opts.callbacks.nodeRightClick.call(this, d.data.name, d.data.extra, d.data.id)
+          opts.callbacks.nodeRightClick.call(
+            this,
+            d.data.name,
+            d.data.extra,
+            d.data.id,
+          );
         }
       });
   }
@@ -207,38 +231,43 @@ class TreeBuilder {
     if (d.target.data.noParent) {
       return 'M0,0L0,0';
     }
-    let ny = Math.round(d.target.y + (d.source.y - d.target.y) * 0.50);
+    let ny = Math.round(d.target.y + (d.source.y - d.target.y) * 0.5);
 
-    let linedata = [{
-      x: d.target.x,
-      y: d.target.y
-    }, {
-      x: d.target.x,
-      y: ny
-    }, {
-      x: d.source.x,
-      y: d.source.y
-    }];
+    let linedata = [
+      {
+        x: d.target.x,
+        y: d.target.y,
+      },
+      {
+        x: d.target.x,
+        y: ny,
+      },
+      {
+        x: d.source.x,
+        y: d.source.y,
+      },
+    ];
 
-    let fun = d3.line().curve(d3.curveStepAfter)
-      .x(function(d) {
+    let fun = d3
+      .line()
+      .curve(d3.curveStepAfter)
+      .x(function (d) {
         return d.x;
       })
-      .y(function(d) {
+      .y(function (d) {
         return d.y;
       });
     return fun(linedata);
   }
 
   _linkSiblings() {
-
     let allNodes = this.allNodes;
 
-    _.forEach(this.siblings, function(d) {
-      let start = allNodes.filter(function(v) {
+    _.forEach(this.siblings, function (d) {
+      let start = allNodes.filter(function (v) {
         return d.source.id == v.data.id;
       });
-      let end = allNodes.filter(function(v) {
+      let end = allNodes.filter(function (v) {
         return d.target.id == v.data.id;
       });
       d.source.x = start[0].x;
@@ -246,54 +275,62 @@ class TreeBuilder {
       d.target.x = end[0].x;
       d.target.y = end[0].y;
 
-      let marriageId = (start[0].data.marriageNode != null ?
-                        start[0].data.marriageNode.id :
-                        end[0].data.marriageNode.id);
-      let marriageNode = allNodes.find(function(n) {
+      let marriageId =
+        start[0].data.marriageNode != null
+          ? start[0].data.marriageNode.id
+          : end[0].data.marriageNode.id;
+      let marriageNode = allNodes.find(function (n) {
         return n.data.id == marriageId;
       });
       d.source.marriageNode = marriageNode;
       d.target.marriageNode = marriageNode;
     });
-
   }
 
   _siblingLine(d, i) {
-
-    let ny = Math.round(d.target.y + (d.source.y - d.target.y) * 0.50);
+    let ny = Math.round(d.target.y + (d.source.y - d.target.y) * 0.5);
     let nodeWidth = this.nodeSize[0];
     let nodeHeight = this.nodeSize[1];
 
     // Not first marriage
     if (d.number > 0) {
-      ny -= Math.round(nodeHeight * 8 / 10);
+      ny -= Math.round((nodeHeight * 8) / 10);
     }
 
-    let linedata = [{
-      x: d.source.x,
-      y: d.source.y
-    }, {
-      x: Math.round(d.source.x + nodeWidth * 6 / 10),
-      y: d.source.y
-    }, {
-      x: Math.round(d.source.x + nodeWidth * 6 / 10),
-      y: ny
-    }, {
-      x: d.target.marriageNode.x,
-      y: ny
-    }, {
-      x: d.target.marriageNode.x,
-      y: d.target.y
-    }, {
-      x: d.target.x,
-      y: d.target.y
-    }];
+    let linedata = [
+      {
+        x: d.source.x,
+        y: d.source.y,
+      },
+      {
+        x: Math.round(d.source.x + (nodeWidth * 6) / 10),
+        y: d.source.y,
+      },
+      {
+        x: Math.round(d.source.x + (nodeWidth * 6) / 10),
+        y: ny,
+      },
+      {
+        x: d.target.marriageNode.x,
+        y: ny,
+      },
+      {
+        x: d.target.marriageNode.x,
+        y: d.target.y,
+      },
+      {
+        x: d.target.x,
+        y: d.target.y,
+      },
+    ];
 
-    let fun = d3.line().curve(d3.curveStepAfter)
-      .x(function(d) {
+    let fun = d3
+      .line()
+      .curve(d3.curveStepAfter)
+      .x(function (d) {
         return d.x;
       })
-      .y(function(d) {
+      .y(function (d) {
         return d.y;
       });
     return fun(linedata);
@@ -309,7 +346,7 @@ class TreeBuilder {
     let tmpSvg = document.createElement('svg');
     document.body.appendChild(tmpSvg);
 
-    _.map(nodes, function(n) {
+    _.map(nodes, function (n) {
       let container = document.createElement('div');
       container.setAttribute('class', n.data.class);
       container.style.visibility = 'hidden';
@@ -335,18 +372,29 @@ class TreeBuilder {
     return [width, maxHeight];
   }
 
-  static _marriageSize (nodes, size) {
+  static _marriageSize(nodes, size) {
     _.map(nodes, function (n) {
       if (!n.data.hidden) {
-        n.cHeight = size
-        n.cWidth = size
+        n.cHeight = size;
+        n.cWidth = size;
       }
-    })
+    });
 
-    return [size, size]
+    return [size, size];
   }
 
-  static _nodeRenderer(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
+  static _nodeRenderer(
+    name,
+    x,
+    y,
+    height,
+    width,
+    extra,
+    id,
+    nodeClass,
+    textClass,
+    textRenderer,
+  ) {
     let node = '';
     node += '<div ';
     node += 'style="height:100%;width:100%;" ';
@@ -367,16 +415,15 @@ class TreeBuilder {
     return node;
   }
 
-  static _marriageRenderer (x, y, height, width, extra, id, nodeClass) {
-    return `<div style="height:100%" class="${nodeClass}" id="node${id}"></div>`
+  static _marriageRenderer(x, y, height, width, extra, id, nodeClass) {
+    return `<div style="height:100%" class="${nodeClass}" id="node${id}"></div>`;
   }
 
   static _debug(msg) {
-    if (TreeBuilder.DEBUG_LEVEL > 0)  {
+    if (TreeBuilder.DEBUG_LEVEL > 0) {
       console.log(msg);
     }
   }
-
 }
 
 export default TreeBuilder;
--- client/src/lib/dtree/dtree.js
+++ client/src/lib/dtree/dtree.js
@@ -1,11 +1,11 @@
+// @ts-nocheck
 import TreeBuilder from './builder.js';
+import _ from 'lodash';
 
 const dTree = {
-
   VERSION: '/* @echo DTREE_VERSION */',
 
-  init: function(data, options = {}) {
-
+  init: function (data, options = {}) {
     var opts = _.defaultsDeep(options || {}, {
       target: '#graph',
       debug: false,
@@ -13,36 +13,67 @@ const dTree = {
       height: 600,
       hideMarriageNodes: true,
       callbacks: {
-        nodeClick: function(name, extra, id) {},
-        nodeRightClick: function(name, extra, id) {},
-        marriageClick: function(extra, id) {},
-        marriageRightClick: function(extra, id) {},
-        nodeHeightSeperation: function(nodeWidth, nodeMaxHeight) {
+        nodeClick: function (name, extra, id) {},
+        nodeRightClick: function (name, extra, id) {},
+        marriageClick: function (extra, id) {},
+        marriageRightClick: function (extra, id) {},
+        nodeHeightSeperation: function (nodeWidth, nodeMaxHeight) {
           return TreeBuilder._nodeHeightSeperation(nodeWidth, nodeMaxHeight);
         },
-        nodeRenderer: function(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
-          return TreeBuilder._nodeRenderer(name, x, y, height, width, extra,
-            id,nodeClass, textClass, textRenderer);
+        nodeRenderer: function (
+          name,
+          x,
+          y,
+          height,
+          width,
+          extra,
+          id,
+          nodeClass,
+          textClass,
+          textRenderer,
+        ) {
+          return TreeBuilder._nodeRenderer(
+            name,
+            x,
+            y,
+            height,
+            width,
+            extra,
+            id,
+            nodeClass,
+            textClass,
+            textRenderer,
+          );
         },
-        nodeSize: function(nodes, width, textRenderer) {
+        nodeSize: function (nodes, width, textRenderer) {
           return TreeBuilder._nodeSize(nodes, width, textRenderer);
         },
-        nodeSorter: function(aName, aExtra, bName, bExtra) {return 0;},
-        textRenderer: function(name, extra, textClass) {
+        nodeSorter: function (aName, aExtra, bName, bExtra) {
+          return 0;
+        },
+        textRenderer: function (name, extra, textClass) {
           return TreeBuilder._textRenderer(name, extra, textClass);
         },
         marriageRenderer: function (x, y, height, width, extra, id, nodeClass) {
-          return TreeBuilder._marriageRenderer(x, y, height, width, extra, id, nodeClass)
+          return TreeBuilder._marriageRenderer(
+            x,
+            y,
+            height,
+            width,
+            extra,
+            id,
+            nodeClass,
+          );
         },
         marriageSize: function (nodes, size) {
-          return TreeBuilder._marriageSize(nodes, size)
+          return TreeBuilder._marriageSize(nodes, size);
         },
       },
       margin: {
         top: 0,
         right: 0,
         bottom: 0,
-        left: 0
+        left: 0,
       },
       nodeWidth: 100,
       marriageNodeSize: 10,
@@ -51,15 +82,15 @@ const dTree = {
         marriageNode: 'marriageNode',
         linage: 'linage',
         marriage: 'marriage',
-        text: 'nodeText'
-      }
+        text: 'nodeText',
+      },
     });
 
     var data = this._preprocess(data, opts);
     var treeBuilder = new TreeBuilder(data.root, data.siblings, opts);
     treeBuilder.create();
 
-    function _zoomTo (x, y, zoom = 1, duration = 500) {
+    function _zoomTo(x, y, zoom = 1, duration = 500) {
       treeBuilder.svg
         .transition()
         .duration(duration)
@@ -68,8 +99,8 @@ const dTree = {
           d3.zoomIdentity
             .translate(opts.width / 2, opts.height / 2)
             .scale(zoom)
-            .translate(-x, -y)
-        )
+            .translate(-x, -y),
+        );
     }
 
     return {
@@ -79,23 +110,23 @@ const dTree = {
           .duration(duration)
           .call(
             treeBuilder.zoom.transform,
-            d3.zoomIdentity.translate(opts.width / 2, opts.margin.top).scale(1)
-          )
+            d3.zoomIdentity.translate(opts.width / 2, opts.margin.top).scale(1),
+          );
       },
       zoomTo: _zoomTo,
       zoomToNode: function (nodeId, zoom = 2, duration = 500) {
-        const node = _.find(treeBuilder.allNodes, {data: {id: nodeId}})
+        const node = _.find(treeBuilder.allNodes, { data: { id: nodeId } });
         if (node) {
-          _zoomTo(node.x, node.y, zoom, duration)
+          _zoomTo(node.x, node.y, zoom, duration);
         }
       },
       zoomToFit: function (duration = 500) {
-        const groupBounds = treeBuilder.g.node().getBBox()
-        const width = groupBounds.width
-        const height = groupBounds.height
-        const fullWidth = treeBuilder.svg.node().clientWidth
-        const fullHeight = treeBuilder.svg.node().clientHeight
-        const scale = 0.95 / Math.max(width / fullWidth, height / fullHeight)
+        const groupBounds = treeBuilder.g.node().getBBox();
+        const width = groupBounds.width;
+        const height = groupBounds.height;
+        const fullWidth = treeBuilder.svg.node().clientWidth;
+        const fullHeight = treeBuilder.svg.node().clientHeight;
+        const scale = 0.95 / Math.max(width / fullWidth, height / fullHeight);
 
         treeBuilder.svg
           .transition()
@@ -105,16 +136,15 @@ const dTree = {
             d3.zoomIdentity
               .translate(
                 fullWidth / 2 - scale * (groupBounds.x + width / 2),
-                fullHeight / 2 - scale * (groupBounds.y + height / 2)
+                fullHeight / 2 - scale * (groupBounds.y + height / 2),
               )
-              .scale(scale)
-          )
-      }
-    }
+              .scale(scale),
+          );
+      },
+    };
   },
 
-  _preprocess: function(data, opts) {
-
+  _preprocess: function (data, opts) {
     var siblings = [];
     var id = 0;
 
@@ -122,11 +152,10 @@ const dTree = {
       name: '',
       id: id++,
       hidden: true,
-      children: []
+      children: [],
     };
 
-    var reconstructTree = function(person, parent) {
-
+    var reconstructTree = function (person, parent) {
       // convert to person to d3 node
       var node = {
         name: person.name,
@@ -135,7 +164,7 @@ const dTree = {
         children: [],
         extra: person.extra,
         textClass: person.textClass ? person.textClass : opts.styles.text,
-        class: person.class ? person.class : opts.styles.node
+        class: person.class ? person.class : opts.styles.node,
       };
 
       // hide linages to the hidden root node
@@ -150,7 +179,7 @@ const dTree = {
           id: id++,
           hidden: true,
           children: [],
-          noParent: node.noParent
+          noParent: node.noParent,
         };
         parent.children.push(pushNode);
         parent = pushNode;
@@ -160,7 +189,7 @@ const dTree = {
       dTree._sortPersons(person.children, opts);
 
       // add "direct" children
-      _.forEach(person.children, function(child) {
+      _.forEach(person.children, function (child) {
         reconstructTree(child, node);
       });
 
@@ -170,7 +199,7 @@ const dTree = {
       dTree._sortMarriages(person.marriages, opts);
 
       // go through marriage
-      _.forEach(person.marriages, function(marriage, index) {
+      _.forEach(person.marriages, function (marriage, index) {
         var m = {
           name: '',
           id: id++,
@@ -179,8 +208,8 @@ const dTree = {
           children: [],
           isMarriage: true,
           extra: marriage.extra,
-          class: marriage.class ? marriage.class : opts.styles.marriageNode
-        }
+          class: marriage.class ? marriage.class : opts.styles.marriageNode,
+        };
 
         var sp = marriage.spouse;
 
@@ -193,60 +222,69 @@ const dTree = {
           textClass: sp.textClass ? sp.textClass : opts.styles.text,
           class: sp.class ? sp.class : opts.styles.node,
           extra: sp.extra,
-          marriageNode: m
+          marriageNode: m,
         };
 
         parent.children.push(m, spouse);
 
         dTree._sortPersons(marriage.children, opts);
-        _.forEach(marriage.children, function(child) {
+        _.forEach(marriage.children, function (child) {
           reconstructTree(child, m);
         });
 
         siblings.push({
           source: {
-            id: node.id
+            id: node.id,
           },
           target: {
-            id: spouse.id
+            id: spouse.id,
           },
-          number: index
+          number: index,
         });
       });
-
     };
 
-    _.forEach(data, function(person) {
+    _.forEach(data, function (person) {
       reconstructTree(person, root);
     });
 
     return {
       root: d3.hierarchy(root),
-      siblings: siblings
+      siblings: siblings,
     };
-
   },
 
-  _sortPersons: function(persons, opts) {
+  _sortPersons: function (persons, opts) {
     if (persons != undefined) {
-      persons.sort(function(a, b) {
-        return opts.callbacks.nodeSorter.call(this, a.name, a.extra, b.name, b.extra);
+      persons.sort(function (a, b) {
+        return opts.callbacks.nodeSorter.call(
+          this,
+          a.name,
+          a.extra,
+          b.name,
+          b.extra,
+        );
       });
     }
     return persons;
   },
 
-  _sortMarriages: function(marriages, opts) {
+  _sortMarriages: function (marriages, opts) {
     if (marriages != undefined && Array.isArray(marriages)) {
-      marriages.sort(function(marriageA, marriageB) {
+      marriages.sort(function (marriageA, marriageB) {
         var a = marriageA.spouse;
         var b = marriageB.spouse;
-        return opts.callbacks.nodeSorter.call(this, a.name, a.extra, b.name, b.extra);
+        return opts.callbacks.nodeSorter.call(
+          this,
+          a.name,
+          a.extra,
+          b.name,
+          b.extra,
+        );
       });
     }
     return marriages;
-  }
-
+  },
 };
 
 export default dTree;
