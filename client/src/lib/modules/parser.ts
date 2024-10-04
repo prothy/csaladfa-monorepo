46d38547e143367e83324db79271eafd3c73932e
Luka Csurgay <lukacsurgay@gmail.com>
2024-10-04 12:15:40 +0200

    wip - add recursive solution to map marriages

--- client/src/lib/modules/parser.ts
+++ client/src/lib/modules/parser.ts
@@ -1,11 +1,10 @@
 import type { DataObject } from 'd3-dtree';
 import { fetchAll } from './api';
 import { calculateDepthOffsets } from '$lib/utils';
+import type { ApiDataNode } from '$lib/types';
 
-export function buildGraph(): DataObject[] {
-  const data = fetchAll();
-
-  const d3Objects: DataObject[] = data.map((item) => ({
+function mapApiObjects(item: ApiDataNode): DataObject {
+  return {
     ...item,
     name: item.id.toString(),
     marriages: item.marriages?.map((marriage) => ({
@@ -15,7 +14,66 @@ export function buildGraph(): DataObject[] {
       children: marriage.children?.map((child) => ({ name: child.toString() })),
     })),
     depthOffset: 0,
-  }));
+  };
+}
 
-  return calculateDepthOffsets(d3Objects);
+function buildTree(mappedDataObjects: DataObject[]): any {
+  const processedNodes = new Set<string>();
+
+  function replaceWithNode(id: string | undefined): any {
+    if (!id || processedNodes.has(id)) {
+      return;
+    }
+
+    processedNodes.add(id);
+    const node = mappedDataObjects.find((n) => n.name === id);
+
+    if (!node) {
+      return;
+    }
+
+    return {
+      ...node,
+      marriages: node.marriages?.map((marriage) => ({
+        spouse: replaceWithNode(marriage.spouse?.name),
+        children: marriage.children?.map((child) =>
+          replaceWithNode(child.name),
+        ),
+      })),
+    };
+  }
+
+  const finalDtreeMap = [];
+
+  mappedDataObjects.forEach((node) => {
+    if (processedNodes.has(node.name)) {
+      return;
+    }
+
+    processedNodes.add(node.name);
+
+    finalDtreeMap.push({
+      ...node,
+      marriages: node.marriages?.map((marriage) => ({
+        spouse: replaceWithNode(marriage.spouse?.name),
+        children: marriage.children?.map((child) =>
+          replaceWithNode(child.name),
+        ),
+      })),
+    });
+  });
+
+  return finalDtreeMap;
+}
+
+export function buildGraph(): DataObject[] {
+  const data = fetchAll();
+
+  const mappedDataObjects: DataObject[] = calculateDepthOffsets(
+    data.map(mapApiObjects),
+  );
+
+  const tree = buildTree(mappedDataObjects);
+
+  return tree;
 }
